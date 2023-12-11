b0d4f868ff46453c1cc7a9a076340064dbf474f4
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-12-11 11:27:45 +0700

    Separate top nav content with it's button

---
+++ lib/components/top_nav_button.dart
@@ -0,0 +1,78 @@
+import 'package:boojek/pages/main_page.dart';
+import 'package:flutter/material.dart';
+
+class TopNavButton extends StatelessWidget {
+  final ValueChanged<MainPageSwitch> updatePageSwitch;
+
+  final MainPageSwitch pageSwitch;
+
+  final String title;
+
+  const TopNavButton({
+    super.key,
+    required this.updatePageSwitch,
+    required this.pageSwitch,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+        child: Container(
+      decoration: _decoration(context, title),
+      child: TextButton(
+          onPressed: () {
+            if (title == 'Beranda') {
+              updatePageSwitch(MainPageSwitch.Home);
+            }
+            if (title == 'Promo') {
+              updatePageSwitch(MainPageSwitch.Promo);
+            }
+            if (title == 'Pesanan') {
+              updatePageSwitch(MainPageSwitch.Orders);
+            }
+            if (title == 'Chat') {
+              updatePageSwitch(MainPageSwitch.Chat);
+            }
+          },
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 12),
+              child: Text(
+                title,
+                style: const TextStyle(fontSize: 14),
+              ),
+            ),
+          )),
+    ));
+  }
+
+  Decoration? _decoration(BuildContext context, String title) {
+    bool selected = false;
+    switch (pageSwitch) {
+      case MainPageSwitch.Home:
+        if (title == 'Beranda') {
+          selected = true;
+        }
+      case MainPageSwitch.Promo:
+        if (title == 'Promo') {
+          selected = true;
+        }
+      case MainPageSwitch.Orders:
+        if (title == 'Pesanan') {
+          selected = true;
+        }
+      case MainPageSwitch.Chat:
+        if (title == 'Chat') {
+          selected = true;
+        }
+    }
+    if (selected) {
+      return BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30),
+      );
+    }
+    return null;
+  }
+}
--- lib/components/top_nav_content.dart
+++ lib/components/top_nav_content.dart
@@ -1,3 +1,4 @@
+import 'package:boojek/components/top_nav_button.dart';
 import 'package:boojek/pages/main_page.dart';
 import 'package:flutter/material.dart';
 
@@ -16,65 +17,12 @@ class TopNavContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        ...['Beranda', 'Promo', 'Pesanan', 'Chat'].map((title) => Flexible(
-          child: Container(
-          decoration: _decoration(context, title),
-          child: TextButton(
-            onPressed: () {
-              if (title == 'Beranda') {
-                updatePageSwitch(MainPageSwitch.Home);
-              }
-              if (title == 'Promo') {
-                updatePageSwitch(MainPageSwitch.Promo);
-              }
-              if (title == 'Pesanan') {
-                updatePageSwitch(MainPageSwitch.Orders);
-              }
-              if (title == 'Chat') {
-                updatePageSwitch(MainPageSwitch.Chat);
-              }
-            },
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 14, vertical: 12),
-                child: Text(
-                  title,
-                  style: const TextStyle(fontSize: 14),
-                ),
-              ),
-            )),
-        )))
+        ...['Beranda', 'Promo', 'Pesanan', 'Chat'].map((title) => TopNavButton(
+            updatePageSwitch: updatePageSwitch,
+            pageSwitch: pageSwitch,
+            title: title
+        ))
       ],
     );
-  }
-
-  Decoration? _decoration(BuildContext context, String title) {
-    bool selected = false;
-    switch (pageSwitch) {
-      case MainPageSwitch.Home:
-        if (title == 'Beranda') {
-          selected = true;
-        }
-      case MainPageSwitch.Promo:
-        if (title == 'Promo') {
-          selected = true;
-        }
-      case MainPageSwitch.Orders:
-        if (title == 'Pesanan') {
-          selected = true;
-        }
-      case MainPageSwitch.Chat:
-        if (title == 'Chat') {
-          selected = true;
-        }
-    }
-    if (selected) {
-      return BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(30),
-      );
-    }
-    return null;
   }
 }
